212f81ff63809663c5b395765590e0f82ec16a68
Suvd Tod <t.suvdaa0424@gmail.com>
2021-09-12 22:41:59 -0500

    added channelRoute

--- routes/messages.js
+++ routes/messages.js
@@ -2,29 +2,35 @@ var express = require('express');
 var router = express.Router();
 const db = require('../models')
 
+// Get all messages
 router.get('/chat', async function(req, res, next) {
+    // find all messages in db
     const messages = await db.Message.findAll({
+        // display username with message
         include: [{
             model: db.User,
             attributes: ['username']
         }]
     })
-
+    // send them back
     res.send(messages)
 })
 
 router.post('/chat', async (req, res) => {
+    // content, message
     if (!req.body.content) {
         res.status(400).json({
             error: 'Must include text before sending'
         })
         return
     }
-
-    const user = await db.User.findByPk(req.session.user.id)
+    // find logged in user
+    const user = await db.Message.findByPk(req.session.user.id)
+    // create a new message
     const message = await user.createMessage({
         content: req.body.content
     })
+    // send back response
     res.json(message)
 })
 
