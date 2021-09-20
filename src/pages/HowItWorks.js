34b3b3a4376f9a42e5afdd633fc664bc0301a88d
marrerap <andrew.marrero@outlook.com>
2021-09-20 10:20:50 -0500

    fixed carousel image size issue as well as the anchor link text size and register page label layout

--- src/pages/HowItWorks.js
+++ src/pages/HowItWorks.js
@@ -12,17 +12,17 @@ function HowItWorks() {
   return (
     <div className="login__page">
          <div className="login__header">
-                <div><a href="/" title="Sign In Page"><img className="header__img" src={Logo} alt=""></img></a></div>
+                <div><a href="/" title="Sign In Page"><img className="header__img Logo" src={Logo} alt=""></img></a></div>
                 <div><a href="/" title="How It Works">SIGN IN</a></div>
-                <div><a href="/register" title="Become A Mentor">BECOME A MENTOR</a></div>
-                <div><a href="/register" title="Register A New User"><button className="register__btn" >BECOME A MENTEE</button></a></div>
+                <div><a href="/register" title="Become A Mentor">BECOME MENTOR</a></div>
+                <div><a href="/register" title="Register A New User"><button className="register__btn" >BECOME MENTEE</button></a></div>
             </div >
 
-            <Container style={{marginBottom: '15px'}} ><h3>How It works:</h3> <br/><span>S.M.A.C. is for and by developers to ask questions and share solutions on software development topics</span></Container>
-      <Carousel >
-        <Carousel.Item>
+            <Container style={{marginBottom: '15px'}} ><h3>How It Works:</h3> <br/><span>S.M.A.C. is for and by developers to ask questions and share solutions on software development topics</span></Container>
+      <Carousel fade >
+        <Carousel.Item className="d-flex justify-content-center">
           <img
-            className="d-block w-100"
+            className="d-block w-65 "
             src={tech}
             alt="First slide"
           />
@@ -31,9 +31,9 @@ function HowItWorks() {
             <p>Meet Developers such as yourself and find solutions together!</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item className="d-flex justify-content-center" >
           <img
-            className="d-block w-100"
+            className="d-block w-65"
             src={collab1}
             alt="Second slide"
           />
@@ -43,9 +43,9 @@ function HowItWorks() {
             <p>Make lifelong connections in the Dev community that is S.M.A.C Overflow!</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item className="d-flex justify-content-center" >
           <img
-            className="d-block w-100"
+            className="d-block w-65"
             src={collab2}
             alt="Third slide"
           />
